0be52c20fd36eaa934d478d5877970b66656dd14
xurror <kaze.nasser@outlook.com>
2021-06-07 11:37:36 +0100

    Setup database connection with mysql

--- scorecardapp/settings.py
+++ scorecardapp/settings.py
@@ -99,6 +99,12 @@ WSGI_APPLICATION = 'scorecardapp.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': 'my.cnf',
+        },
+    },
+    'sqlite3': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
