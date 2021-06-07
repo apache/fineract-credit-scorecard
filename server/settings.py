0272d9c1bccf260ffb7b5814493e074b8737cc90
xurror <kaze.nasser@outlook.com>
2021-06-07 12:10:20 +0100

    Rename mysql connection conf file

--- server/settings.py
+++ server/settings.py
@@ -102,7 +102,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {
-            'read_default_file': 'my.cnf',
+            'read_default_file': 'mysql.cnf',
         },
     },
     'sqlite3': {
