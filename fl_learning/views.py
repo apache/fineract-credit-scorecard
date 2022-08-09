aa95fee8d832ca09a8498aa0d9783718b50ef29d
Ziwei <ziweihans@gmail.com>
2022-08-09 17:05:23 -0400

    add predict api

--- fl_learning/views.py
+++ fl_learning/views.py
@@ -17,21 +17,26 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from fl_learning.fl_utility import preprocessing
-from fl_learning.fl_model import classifier_train, Net, accuracy,ClassifierClient
+from fl_learning.fl_model import classifier_train, Net, accuracy, ClassifierClient
+from ml.classifiers import RandomForestClassifier, MLP
 
 
 class FLModelTrainAPIView(APIView):
 
     def post(self, request):
+        # ------------------ read data ---------------------
         addr = request.data['addr']
         print(addr)
         try:
-            data = pd.read_csv(addr)
+            data = pd.read_csv(addr, usecols=['Age', 'Sex', 'Job', 'Housing', 'Saving accounts', 'Checking account',
+                                              'Credit amount', 'Duration', 'Purpose', 'Risk'])
         except Exception as e:
             raise APIException(str(e))
         print(data.head())
+
+        # ------------------ connect to server ---------------------
         IP = '127.0.0.1'
-        SERVER_PORT = 50000
+        SERVER_PORT = 51000
         BUFLEN = 1024
 
         dataSocket = socket(AF_INET, SOCK_STREAM)
@@ -40,25 +45,24 @@ class FLModelTrainAPIView(APIView):
         except:
             raise APIException("Unsuccessfully connect to the server, check the address and port")
 
-
+        # ------------------ Train ---------------------------------
         command = "Train"
         dataSocket.send(command.encode())
-        # Preparing Data
-        german = pd.read_csv(addr)
-        test_size = 0.20
-        # processed_data = None
-        # categorical = None
-        # label_encoders = {}
 
-        X_train, X_test, y_train, y_test = preprocessing("German", german, test_size)
+        test_size = 0.01
+        X_train, X_test, y_train, y_test = preprocessing("German", data, test_size)
         X_train_tensor = t.FloatTensor(X_train)
         y_train_tensor = t.tensor(y_train, dtype=t.long)
         X_test_tensor = t.FloatTensor(X_test)
         y_test_tensor = t.tensor(y_test, dtype=t.long)
 
+        print(X_train[0])
+
         # Define the input and output
         input_channels = X_train_tensor.shape[1]
         output_channels = 2
+
+        print(input_channels)
 
         # Initialize the model
         net = Net(input_channels, output_channels)
@@ -69,9 +73,14 @@ class FLModelTrainAPIView(APIView):
         # Number of epochs
         epochs = 5000
 
+        client = ClassifierClient(net, X_train_tensor, y_train_tensor, criterion, optimizer, epochs)
+
         print(">>> Connect Flower server")
-        client = ClassifierClient(net,X_train_tensor,y_train_tensor,criterion,optimizer,epochs)
         fl.client.start_numpy_client("[::]:8085", client=client)
+
+        # for name, param in net.named_parameters():
+        #     if param.requires_grad:
+        #         print(name, param.data)
 
         print(">>> Send Model to Server")
         serialized_model = pickle.dumps(net.state_dict())
@@ -84,6 +93,63 @@ class FLModelTrainAPIView(APIView):
 
 class FLModelPredictAPIView(APIView):
     def post(self, request):
-        addr = request.data['addr']
-        print(addr)
+        # Data Input Format:
+        age = request.data['age']
+        sex = request.data['sex']
+        job = request.data['job']
+        housing = request.data['housing']
+        credit = request.data['credit']
+        duration = request.data['duration']
+        purpose = request.data['purpose']
+
+        data = {
+            "age": int(age),
+            "sex": sex,
+            "job": int(job),
+            "housing": housing,
+            "credit_amount": int(credit),
+            "duration": int(duration),
+            "purpose": purpose
+        }
+
+        # test_data = {
+        #     "age": 22,
+        #     "sex": "female",
+        #     "job": 2,
+        #     "housing": "own",
+        #     "credit_amount": 5951,
+        #     "duration": 48,
+        #     "purpose": "radio/TV"
+        # }
+
+        my_alg = MLP()  # only use for processing the data
+        data = my_alg.preprocessing(data).to_numpy()
+        print(data)
+        data_tensor = t.FloatTensor(data)
+
+        IP = '127.0.0.1'
+        SERVER_PORT = 51000
+        BUFLEN = 1024
+
+        dataSocket = socket(AF_INET, SOCK_STREAM)
+        try:
+            dataSocket.connect((IP, SERVER_PORT))
+        except:
+            raise APIException("Unsuccessfully connect to the server, check the address and port")
+
+        command = "Predict"
+        dataSocket.send(command.encode())
+
+        new_model_dict_string = dataSocket.recv(BUFLEN * 100)
+        new_model_dict = pickle.loads(new_model_dict_string)
+        net = Net(7, 2)
+        net.load_state_dict(new_model_dict)
+        with t.no_grad():
+            net.eval()
+            y_pred_vector = net(data_tensor)
+            # val_loss = criterion(y_pred_vector, y_test_tensor)
+            y_pred = net.predict(data_tensor)
+            # acc = (y_test_tensor == y_pred).sum() / y_test_tensor.size(0)
+        print("Prediction vector:", y_pred_vector)
+        print("Prediction Result:", y_pred)
         return Response("Successfully")
